0427b7a2211fa49526657ef1dbbaf01bde6a2909
zhangrujian <zhangrujian@mysteel.com>
2022-01-17 19:30:28 +0800

    测试合并代码解决异常

---
+++ src/main/java/com/test/Test1.java
@@ -0,0 +1,19 @@
+package com.test;
+
+/**
+ * @Author ZhangRuJian
+ * @Data 2022/1/17
+ */
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaa");
+    }
+
+}
